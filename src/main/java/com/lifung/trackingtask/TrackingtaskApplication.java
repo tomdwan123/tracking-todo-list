99069ff6044667e574a05e77f1d07197c0ffd9dd
tomdwan123 <tomdwanno1@gmail.com>
2023-06-26 12:22:32 +0700

    ✨ Main: Init project TODO List

---
+++ src/main/java/com/lifung/trackingtask/TrackingtaskApplication.java
@@ -0,0 +1,13 @@
+package com.lifung.trackingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifung/trackingtask/TrackingtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifung.trackingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
